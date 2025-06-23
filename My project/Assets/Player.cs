279def7bd824a36713713bbf20021ad0f42b1374
EllieWasd <medeiros.elisa@escolar.ifrn.edu.br>
2025-06-23 09:56:39 -0300

    comentario

---
+++ My project/Assets/Player.cs
@@ -0,0 +1,41 @@
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    
+    public float velocidade = 10;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+              
+    void Update()
+    {
+        Vector3 posicao = transform.position;
+        //esquerda X-
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            posicao.x = transform.position.x - velocidade * Time.captureDeltaTime;
+        }
+        
+        //direita x+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            posicao.x = transform.position.x + velocidade * Time.captureDeltaTime;
+        }
+        
+        //cima Z+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            
+        }
+        
+        //baixo Z-
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+     
+        }
+
+        transform.position = posicao;
+    }
+}
